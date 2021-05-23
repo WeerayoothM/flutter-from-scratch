5da8d2e012d32f24500a269c031abaac80d7797f
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-05-23 15:21:03 +0700

    use container

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "My App",
       home: MyHomePage(),
-      theme: ThemeData(primarySwatch: Colors.pink),
+      theme: ThemeData(primarySwatch: Colors.lightGreen),
     );
   }
 }
@@ -22,135 +22,143 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int number = 0; // create state
-
-  // menus
-  List<FoodMenu> menu = [
-    FoodMenu('กุ้งเผา', '500',
-        '../assets/images/1584346965_685720fc428113fefffeaff8e7a02f99.jpeg'),
-    FoodMenu('กะเพราหมูสับ', '60',
-        '../assets/images/8ef21e7e11312bcb0af4809098025695.jpeg'),
-    FoodMenu('ส้มตำ', '50',
-        '../assets/images/7E68135B4E03435881B219B331FB9169.jpeg'),
-  ];
-
 // แสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
-    List<Widget> data = [];
-    data.add(Text(
-      'กดปุ่มเพื่อเพิ่มตัวเลข',
-      style: TextStyle(fontSize: 20),
-    ));
-    data.add(Text(
-      number.toString(),
-      style: TextStyle(fontSize: 60),
-    ));
-    for (var i = 0; i < 10; i++) {
-      data.add(Text('รายการที่ ${i + 1}'));
-    }
     return Scaffold(
       appBar: AppBar(
-        title: Text('เลือกเมนูอาหาร'),
+        title: Text(
+          'บัญชีรายรับรายจ่าย',
+          style: TextStyle(
+              fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         centerTitle: false,
       ),
-      body: ListView.builder(
-          itemCount: menu.length,
-          itemBuilder: (BuildContext context, int index) {
-            FoodMenu food = menu[index]; // food is Object
-            return ListTile(
-              leading: Image.asset(food.imgFood),
-              title: Text(
-                food.name,
-                style: TextStyle(fontSize: 20),
+      body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 130,
+              decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(8)),
+              // border: Border.all(width: 2, color: Colors.pink),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'ยอดคงเหลือ',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '฿32,000',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
               ),
-              subtitle: Text(
-                'ราคา ${food.price} บาท',
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: BorderRadius.circular(8)),
+              // border: Border.all(width: 2, color: Colors.pink),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'รายรับ',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                      child: Text(
+                    '฿15,000',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ))
+                ],
               ),
-              onTap: () {
-                print('คุณเลือกเมนูอาหารชื่อ ${food.name}');
-              },
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: addNumber,
-        child: Icon(Icons.add),
-      ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                  color: Colors.orange, borderRadius: BorderRadius.circular(8)),
+              // border: Border.all(width: 2, color: Colors.pink),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'รายจ่าย',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                      child: Text(
+                    '฿5,000',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ))
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 100,
+              decoration: BoxDecoration(
+                  color: Colors.orange, borderRadius: BorderRadius.circular(8)),
+              // border: Border.all(width: 2, color: Colors.pink),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'ค้างชำระ',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                      child: Text(
+                    '฿5,000',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ))
+                ],
+              ),
+            )
+          ])),
     );
-  }
-
-  void addNumber() {
-    setState(() {
-      number = number + 1;
-    });
-  }
-
-  void decreaseNumber() {
-    if (number > 0) {
-      setState(() {
-        number = number - 1;
-      });
-    }
-  }
-
-// เตรียมข้อมูล return type with List
-  List<Widget> getData(int count) {
-    List<Widget> data = [];
-    for (var i = 0; i < count; i++) {
-      if (i == count - 1) {
-        data.add(Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: TextButton(
-                    child: Icon(Icons.add),
-                    onPressed: addNumber,
-                  ),
-                ),
-                Text(
-                  number.toString(),
-                  style: TextStyle(fontSize: 40),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: TextButton(
-                    child: Icon(Icons.delete),
-                    onPressed: decreaseNumber,
-                  ),
-                ),
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            Text(
-              'Hello',
-              style: TextStyle(fontSize: 20),
-            ),
-            TextButton(
-              child: Text(
-                'สวัสดีครับ',
-                style: TextStyle(color: Colors.amber, fontSize: 30),
-              ),
-              onPressed: () => print('button'),
-            ),
-            Image(
-                image: NetworkImage(
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Gatto_europeo4.jpg/250px-Gatto_europeo4.jpg'))
-          ],
-        ));
-        break;
-      }
-      var menu = ListTile(
-        title: Text(
-          'เมนูที่ ${i + 1}',
-        ),
-        subtitle: Text('หัวข้อย่อยที่ ${i + 1}'),
-      );
-
-      data.add(menu);
-    }
-    return data;
   }
 }
